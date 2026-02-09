a52583d7743143aa06c2fc163d62aaec84325819
Skyetiz <zhouxy2602@outlook.com>
2026-02-09 14:35:16 +0800

    update1

---
+++ My/webpacks/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+
+module.exports = {
+    entry:"./src/index.ts",
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    
+    module: {
+        rules: [
+            {   
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            }
+        ]  
+    },
+    mode: 'production'
+}
